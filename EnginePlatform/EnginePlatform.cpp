c5449b990d3a5b8ea81b0abee571668d15cb07ed
김수원 <rla5233@naver.com>
2024-11-29 19:14:54 +0900

    프로젝트 종속성 설정, 프로젝트 참조 설정, 프로젝트 구성 형식 설정 업데이트

--- EnginePlatform/EnginePlatform.cpp
+++ EnginePlatform/EnginePlatform.cpp
@@ -1,7 +1,4 @@
-﻿// EnginePlatform.cpp : 애플리케이션에 대한 진입점을 정의합니다.
-//
-
-#include "framework.h"
+﻿#include "framework.h"
 #include "EnginePlatform.h"
 
 #define MAX_LOADSTRING 100
@@ -38,11 +35,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         return FALSE;
     }
 
+    // 단축키 설정
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_ENGINEPLATFORM));
 
     MSG msg;
 
-    // 기본 메시지 루프입니다:
+    // 기본 메시지 루프입니다
     while (GetMessage(&msg, nullptr, 0, 0))
     {
         if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
